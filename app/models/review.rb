be909fb52adf819753fde063df019b983f772140
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-13 11:47:48 +0100

    ok for validations in models

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :restaurant_id, presence: true
+  validates :content, presence: true, allow_blank: false
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, allow_blank: false, numericality: { only_integer: true }
+end
