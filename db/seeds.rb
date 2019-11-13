aef163fbe49bf559cbaa63476b81995918f15ab2
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-13 14:28:19 +0100

    fake yelp ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.new(
+  name: "la tour d'argent",
+  phone_number: '04 93 45 45 45',
+  address: '3 rue de Rivoli',
+  category: 'chinese'
+)
+
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: "le lion d'or",
+  phone_number: '04 93 45 45 46',
+  address: '3 rue de Bretagne',
+  category: 'french'
+)
+
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: "chez michel",
+  phone_number: '04 93 45 45 47',
+  address: '3 rue du Bac',
+  category: 'belgian'
+)
+
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: "chez jeanne",
+  phone_number: '04 93 45 45 64',
+  address: '3 rue Saint-Maur',
+  category: 'japanese'
+)
+
+restaurant.save
+
+restaurant = Restaurant.new(
+  name: "Krugen",
+  phone_number: '04 93 45 65 64',
+  address: '3 rue de la fontaine au roi',
+  category: 'french'
+)
+
+restaurant.save
